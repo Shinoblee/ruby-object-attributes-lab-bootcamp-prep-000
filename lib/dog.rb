fce5ce7cbbe7714969058c2385d2ca80bf1b1b93
Lee Simon <shinoblee@gmail.com>
2018-09-26 11:13:10 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,13 @@
 class Dog 
-
   
-
-
-end
   def initialize(name)
-    @name = name
+    @name = (name)
   end
- 
+  
   def name
     @name
   end
+
+
+end
  
-  def name=(new_name)
-    @name = new_name
-  end
